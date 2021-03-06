28128acb8cd7114a30c9ee0bc47d85bc7b0c095c
ausarenglish <ausareenglish@gmail.com>
2021-03-06 11:22:13 -0500

    profile & match boilerplate added

--- client/App.jsx
+++ client/App.jsx
@@ -2,19 +2,10 @@ import React, { Component, useState, useEffect} from "react";
 import { Switch, Route, Link, withRouter } from "react-router-dom";
 import Login from './pages/login.jsx';
 
+
 const App = () => {
   return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+      <div style = {mainStyles.container}>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -25,7 +16,30 @@ const App = () => {
           <Route path="/">
             <Home />
           </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/match">
+            <Match />
+          </Route>
         </Switch>
+
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                <Link to="/match">Match</Link>
+                </li>
+              </ul>
+            </nav>
       </div>
   );
 }
@@ -33,5 +47,27 @@ const App = () => {
 function Home() {
   return <h2>Home</h2>;
 }
+
+function Profile() {
+  return <h2>Profile</h2>;
+}
+
+function Match() {
+  return <h2>Match</h2>;
+}
+
+
+
+
+// styles //
+
+// styles for mainContainer
+const mainStyles = {
+  container: {
+    maxWidth: '1200px',
+  }
+}
+
+
 
 export default withRouter(App);
